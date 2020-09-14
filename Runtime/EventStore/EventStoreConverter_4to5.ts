7a1aee8a2a8a59bb2f7b3c11c12c6e6b2a79f1d2
Einar Ingebrigtsen <einar@dolittle.com>
2020-09-14 20:08:29 +0200

    Cleanup - separated out

---
+++ Runtime/EventStore/EventStoreConverter_4to5.ts
@@ -0,0 +1,171 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { Connection, Model, Document } from 'mongoose';
+import { getCommitModelFor, Commit } from './v4/Commit';
+import { Event as v4Event } from './v4/Event';
+import { getEventModelFor, Event } from './v5/Event';
+import { Guid } from '@dolittle/rudiments';
+import { Binary } from 'mongodb';
+import { getAggregateVersionModelFor, AggregateVersion } from './v5/AggregateVersion';
+import { mongoose } from '@typegoose/typegoose';
+import { ILogger } from './ILogger';
+
+export class EventStoreConverter {
+    private _commitModel: Model<Document>;
+    private _eventModel: Model<Document>;
+    private _aggregateModel: Model<Document>;
+
+    constructor(
+        private readonly _sourceConnection: Connection,
+        private readonly _destinationConnection: Connection,
+        private readonly _logger: ILogger) {
+        this._commitModel = getCommitModelFor(_sourceConnection);
+        this._eventModel = getEventModelFor(_destinationConnection);
+        this._aggregateModel = getAggregateVersionModelFor(_destinationConnection);
+    }
+
+    async convert() {
+        this.dropExistingCollections();
+
+        const total = await this._commitModel.countDocuments();
+        this._logger.info(`Starting to convert ${total} commits into events`);
+
+        const cursor = await this._commitModel.find().cursor();
+
+        let sequenceNumber = 0;
+
+        for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+            const commit = doc as Commit;
+
+            for (const sourceEvent of commit.events) {
+                const destinationEvent = new Event();
+                destinationEvent._id = sequenceNumber
+                destinationEvent.ExecutionContext = this.getExecutionContextFrom(sourceEvent)
+                destinationEvent.Metadata = this.getMetadataFrom(sourceEvent, sequenceNumber);
+                destinationEvent.Aggregate = this.getAggregateFrom(sourceEvent);
+
+                destinationEvent.EventHorizon = this.getEmptyEventHorizon();
+
+                destinationEvent.Content = {};
+
+                this.getContentFrom(sourceEvent);
+
+                destinationEvent.Aggregate.Version = await this.getAggregateVersionFor(sourceEvent);
+
+                await this._eventModel.create(destinationEvent);
+
+                process.stdout.write('.');
+
+                sequenceNumber++;
+            }
+        }
+    }
+
+    private getContentFrom(sourceEvent: v4Event) {
+        const content: any = {};
+        for (const property in sourceEvent.event) {
+            const camelCasePropertyName = property.toCamelCase();
+            const value = sourceEvent.event[property];
+            if (value instanceof Binary) {
+                const binaryValue = value as Binary;
+                if (binaryValue.buffer.length === 16) {
+                    const guid = binaryValue.toGuid();
+                    content[camelCasePropertyName] = guid.toString();
+                }
+                else {
+                    let base64String = btoa(String.fromCharCode(...new Uint8Array(binaryValue.buffer)));
+                    content[camelCasePropertyName] = base64String;
+                }
+            }
+            else {
+                content[camelCasePropertyName] = value.toString();
+            }
+        }
+        return content;
+    }
+
+    private getMetadataFrom(sourceEvent: v4Event, sequenceNumber: number) {
+        return {
+            Occurred: new Date(sourceEvent.occurred),
+            EventLogSequenceNumber: sequenceNumber,
+            EventSource: sourceEvent.eventsource_id,
+            TypeId: sourceEvent.event_artifact,
+            TypeGeneration: 1,
+            Public: false
+        };
+    }
+
+    private getExecutionContextFrom(sourceEvent: v4Event) {
+        return {
+            Correlation: sourceEvent.correlation_id,
+            Microservice: sourceEvent.original_context.bounded_context,
+            Tenant: sourceEvent.original_context.tenant,
+            Version: {
+                Major: 0,
+                Minor: 0,
+                Patch: 0,
+                Build: 0,
+                PreRelease: ''
+            },
+            Environment: sourceEvent.original_context.environment,
+            Claims: []
+        };
+    }
+
+    private getAggregateFrom(sourceEvent: v4Event) {
+        return {
+            WasAppliedByAggregate: true,
+            TypeId: sourceEvent.event_source_artifact,
+            TypeGeneration: 1,
+            Version: 0
+        };        
+    }
+
+    private getEmptyEventHorizon() {
+        return {
+            FromEventHorizon: false,
+            ExternalEventLogSequenceNumber: 0,
+            Received: new Date(0),
+            Consent: Guid.empty
+        };        
+    }
+
+    private async getAggregateVersionFor(sourceEvent: v4Event): Promise<number> {
+
+        const aggregateVersionCriteria = {
+            AggregateType: sourceEvent.event_source_artifact,
+            EventSource: sourceEvent.eventsource_id
+        };
+        const hasAggregate = await this._aggregateModel.exists(aggregateVersionCriteria);
+
+        let version: number = 1;
+
+        if (!hasAggregate) {
+            await this._aggregateModel.create({
+                ...aggregateVersionCriteria,
+                ...{
+                    _id: new mongoose.mongo.ObjectId(),
+                    Version: 1
+                }
+            });
+        } else {
+            await this._aggregateModel.update(
+                aggregateVersionCriteria,
+                { $inc: { Version: 1 } }
+            );
+
+            const aggregateVersionDocument = await this._aggregateModel.findOne(aggregateVersionCriteria).exec();
+            if (aggregateVersionDocument) {
+                version = (aggregateVersionDocument as any as AggregateVersion).Version;
+            }
+        }
+
+        return version;
+    }
+
+    private dropExistingCollections() {
+        this._destinationConnection.dropCollection('event-log');
+        this._destinationConnection.dropCollection('aggregates');
+    }
+}
--- Runtime/EventStore/index.ts
+++ Runtime/EventStore/index.ts
@@ -5,25 +5,13 @@ import mongoose from 'mongoose';
 import { configureLogging } from './Logging';
 import { Container } from 'typescript-ioc';
 import { ILogger } from './ILogger';
-
-import { CommitModel, getCommitModelFor, Commit } from './v4/Commit';
-import { getEventModelFor, Event } from './v5/Event';
-import { Guid } from '@dolittle/rudiments';
-import { Binary } from 'mongodb';
 import './StringExtensions';
-import { getAggregateVersionModelFor, AggregateVersion } from './v5/AggregateVersion';
-import { ObjectId } from 'mongodb';
+import { EventStoreConverter } from './EventStoreConverter_4to5';
 
 configureLogging();
 const logger = Container.get(ILogger);
 
-
 (async () => {
-
-    // Aggregate Version - v5
-
-    logger.info('Connect');
-
     try {
         const sourceConnection = await mongoose.createConnection('mongodb://localhost:27018', {
             useNewUrlParser: true,
@@ -39,125 +27,11 @@ const logger = Container.get(ILogger);
             useFindAndModify: false
         });
 
-        for (const collection in destinationConnection.collections) {
-            logger.info(`Dropping ${collection}`);
-            //destinationConnection.dropCollection(collection);
-        }
-
-        destinationConnection.dropCollection('event-log');
-        destinationConnection.dropCollection('aggregates');
-
         logger.info('Connected');
-        const commitModel = getCommitModelFor(sourceConnection);
-        const eventModel = getEventModelFor(destinationConnection);
-        const aggregateModel = getAggregateVersionModelFor(destinationConnection);
 
-        const total = await commitModel.countDocuments();
-        logger.info(`Starting to convert ${total} commits into events`);
-        const cursor = await commitModel.find().cursor();
+        const converter = new EventStoreConverter(sourceConnection, destinationConnection, logger);
+        await converter.convert();
 
-        let sequenceNumber = 0;
-
-        for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-            const commit = doc as Commit;
-
-            for (const sourceEvent of commit.events) {
-                const destinationEvent = new Event();
-                destinationEvent._id = sequenceNumber
-                destinationEvent.ExecutionContext = {
-                    Correlation: sourceEvent.correlation_id,
-                    Microservice: sourceEvent.original_context.bounded_context,
-                    Tenant: sourceEvent.original_context.tenant,
-                    Version: {
-                        Major: 0,
-                        Minor: 0,
-                        Patch: 0,
-                        Build: 0,
-                        PreRelease: ''
-                    },
-                    Environment: sourceEvent.original_context.environment,
-                    Claims: []
-                };
-
-                destinationEvent.Metadata = {
-                    Occurred: new Date(sourceEvent.occurred),
-                    EventLogSequenceNumber: sequenceNumber,
-                    EventSource: sourceEvent.eventsource_id,
-                    TypeId: sourceEvent.event_artifact,
-                    TypeGeneration: 1,
-                    Public: false
-                };
-
-                destinationEvent.Aggregate = {
-                    WasAppliedByAggregate: true,
-                    TypeId: sourceEvent.event_source_artifact,
-                    TypeGeneration: 1,
-                    Version: 0
-                };
-
-                destinationEvent.EventHorizon = {
-                    FromEventHorizon: false,
-                    ExternalEventLogSequenceNumber: 0,
-                    Received: new Date(0),
-                    Consent: Guid.empty
-                };
-
-                destinationEvent.Content = {};
-
-                for (const property in sourceEvent.event) {
-                    const camelCasePropertyName = property.toCamelCase();
-                    const value = sourceEvent.event[property];
-                    if (value instanceof Binary) {
-                        const binaryValue = value as Binary;
-                        if (binaryValue.buffer.length === 16) {
-                            const guid = binaryValue.toGuid();
-                            destinationEvent.Content[camelCasePropertyName] = guid.toString();
-                        } else {
-                            let base64String = btoa(String.fromCharCode(...new Uint8Array(binaryValue.buffer)));
-                            destinationEvent.Content[camelCasePropertyName] = base64String;
-                        }
-                    } else {
-                        destinationEvent.Content[camelCasePropertyName] = value.toString();
-                    }
-                }
-
-                const aggregateVersionCriteria = {
-                    AggregateType: sourceEvent.event_source_artifact,
-                    EventSource: sourceEvent.eventsource_id
-                };
-                const hasAggregate = await aggregateModel.exists(aggregateVersionCriteria);
-
-                let version: number = 1;
-
-                if (!hasAggregate) {
-                    await aggregateModel.create({
-                        ...aggregateVersionCriteria,
-                        ...{
-                            _id: new mongoose.mongo.ObjectId(),
-                            Version: 1
-                        }
-                    });
-                } else {
-                    await aggregateModel.update(
-                        aggregateVersionCriteria,
-                        { $inc: { Version: 1 } }
-                    );
-
-                    const aggregateVersionDocument = await aggregateModel.findOne(aggregateVersionCriteria).exec();
-                    if (aggregateVersionDocument) {
-                        version = (aggregateVersionDocument as any as AggregateVersion).Version;
-                    }
-                }
-
-                destinationEvent.Aggregate.Version = version;
-
-                await eventModel.create(destinationEvent);
-
-                process.stdout.write('.');
-
-                sequenceNumber++;
-            }
-        }
     } catch (e) {
         logger.error(`Couldn't connect to Mongo : '${e}'`);
     }
