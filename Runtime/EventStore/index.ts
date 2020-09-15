ae24b94a9f9045c6af7a652110204165188ff599
Einar Ingebrigtsen <einar@dolittle.com>
2020-09-15 09:48:21 +0200

    Disabling the automatic run of converter

--- Runtime/EventStore/index.ts
+++ Runtime/EventStore/index.ts
@@ -1,18 +1,20 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import mongoose from 'mongoose';
+export { EventStoreConverter } from './EventStoreConverter';
+export { ILogger } from './ILogger';
+
 import { configureLogging } from './Logging';
 import { Container } from 'typescript-ioc';
 import { ILogger } from './ILogger';
 import './StringExtensions';
-import { EventStoreConverter } from './EventStoreConverter';
-
-export { EventStoreConverter } from './EventStoreConverter';
-export { ILogger } from './ILogger';
 
 configureLogging();
 export const logger = Container.get(ILogger);
+
+/*
+import mongoose from 'mongoose';
+import { EventStoreConverter } from './EventStoreConverter';
 
 (async () => {
     try {
@@ -39,3 +41,4 @@ export const logger = Container.get(ILogger);
         logger.error(`Couldn't connect to Mongo : '${e}'`);
     }
 })();
+*/
